e250edebb4301b98402ddff6a4a21cd6514dcc13
annachen88 <annachen880522@gmail.com>
2017-12-12 16:25:07 +0800

    GameTester

--- myproject2/src/com/ana/oo/Student.java
+++ myproject2/src/com/ana/oo/Student.java
@@ -5,21 +5,27 @@ public class Student {
 	int english;
 	int math;
 	int chinese;
+	String name;
 
-		public void print(){
-		System.out.println(english+"\t"+
-		math+"\t"+chinese);	
-			//設計一個方法
-		}
-		public Student(int e,int m,int c)
-		{english =e;
-		math =m;
-		chinese =c;
-			//建構子
-			
-		}
-		public int getAverge(){
-			//要回傳
-			return(english+math+chinese)/3;
-		}
+	public void print() {
+		System.out.println("Justin :" + english + "\t" + math + "\t" + chinese);
+		// 設計一個方法
+	}
+
+	public Student(int english, int math, int chinese) {
+		this.english = english;
+		this.math = math;
+		this.chinese = chinese;
+		// 建構子
+	}
+
+	public Student(String name) {
+		this.name = name;
+	}
+
+	// 建構子
+	public int getAverge() {
+		// 要回傳
+		return (english + math + chinese) / 3;
+	}
 }
