9aadcb248c6a9ca80e5cc7b3f8fbff55b866da5d
annachen88 <annachen880522@gmail.com>
2018-01-09 16:30:26 +0800

    arraylist

--- myproject2/src/com/ana/customer/SliverCustomer.java
+++ myproject2/src/com/ana/customer/SliverCustomer.java
@@ -3,13 +3,11 @@ package com.ana.customer;
 public class SliverCustomer extends Customer {
 
 	public SliverCustomer(int amount) {
+		super(amount);
+		super.discount = 0.1f;
 		
-		super.discount=0.1f;;
-		
+
 	}
-	@Overload
-	public void print(){
-		int total=(int)(amount*(1-discount));
-		System.out.println("原價:"+amount+"	"+"打折後:"+total);
-	}
+
+	
 }
---
+++ myproject2/src/com/ana/ticket/Tester.java
@@ -0,0 +1,10 @@
+package com.ana.ticket;
+
+public class Tester {
+
+	public static void main(String[] args) {
+    Ticket t1 =new Ticket("2018.01.18","18:30","松山","八堵","65");
+    t1.print();
+	}
+
+}
