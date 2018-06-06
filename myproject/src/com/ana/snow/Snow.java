9a33a81a8045e7b43e48507d28a37045113591df
annachen88 <annachen880522@gmail.com>
2018-06-06 23:51:05 +0800

    change error

--- myproject/src/com/ana/snow/Snow.java
+++ myproject/src/com/ana/snow/Snow.java
@@ -3,7 +3,7 @@ package com.ana.snow;
 import java.util.Random;
 
 public class Snow extends Thread {
-<<<<<<< HEAD
+
 	Random random = new Random();
 	int x;
 	int y = 800;
@@ -25,26 +25,3 @@ public class Snow extends Thread {
 
 	
 }
-=======
-	int x;
-	int y = 800;
-	Random random = new Random();
-
-	public Snow() {
-		x = random.nextInt(600) + 1;
-	}
-
-	@Override
-	public void run() {
-		for (int i = 800; i > 0; i--) {
-			//System.out.println(getName() + "(" + x + " " + y + ")");
-			y = y - 1;
-			int movement = random.nextInt(3) - 1;
-			x = x - movement;
-			System.out.println(new String(new char[x]).replace(" ", " "));
-			System.out.println("o");
-		}
-	}
-
-}
->>>>>>> 148c01786eefd7ee0971451f4463326a93f16eb4
